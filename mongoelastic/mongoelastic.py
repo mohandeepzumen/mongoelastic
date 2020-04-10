4bee81830da9232e8c7cd3931d811d93218bd949
Naim Malek <naimmalek7600@gmail.com>
2020-04-10 14:44:55 +0530

    Improvement

--- mongoelastic/mongoelastic.py
+++ mongoelastic/mongoelastic.py
@@ -8,17 +8,18 @@ class MongoElastic:
 
         # mongo db connection
         self.mongo_host = args.get('mongo_host', 'localhost')
-        self.mongo_port = args.get('mongo_port', 27017)
+        self.mongo_port = args.get('mongo_port', None)
         self.mongo_max_pool_size = args.get('mongo_max_pool_size', 50)
         self.mongo_db_name = args.get('mongo_db_name', None)
         self.mongo_document_name = args.get('mongo_document_name', None)
+
         # elasticsearch connection
         self.es_host = args.get('es_host', ['localhost'])
-        self.es_http_auth = args.get('es_http_auth', ('elastic', 'secret'))
-        self.es_port = args.get('es_port', 9200)
+        self.es_http_auth = args.get('es_http_auth', None)
+        self.es_port = args.get('es_port', None)
         self.es_index_name = args.get('es_index_name', 'mongoelastic_index')
         self.es_doc_type = args.get('es_doc_type', 'mongoelastic_doc_type')
-
+    
     def start(self, m_filter=None):
         m_filter = dict() if not m_filter else m_filter
         client = MongoClient(self.mongo_host, self.mongo_port, maxPoolSize=self.mongo_max_pool_size)
@@ -28,12 +29,17 @@ class MongoElastic:
         mongo_where = m_filter.get('mongo_condition', {})
         # get all data from mongoDB db
         m_data = document_name.find(mongo_where)
+
+        conf = dict()
+        if self.es_http_auth:
+            conf['http_auth'] = self.es_http_auth
+        if self.es_port:
+            conf['port'] = self.es_port
+        conf['use_ssl'] = False
         es = Elasticsearch(
             self.es_host,
-            http_auth=self.es_http_auth,
-            port=self.es_port,
-            use_ssl=False)
-
+            **conf
+        )
         i = 1
         spinner = Spinner('Importing... ')
         for line in m_data:
